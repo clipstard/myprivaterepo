b158632af16f841b5acf9ee2e97c9f6e069be784
Nicolenco EugeniuN <eugeniu.nicolenco@tekoway.com>
2022-03-28 18:09:39 +0300

    ajax example

--- actions.js
+++ actions.js
@@ -1,3 +1,8 @@
-$(document).ready(() => {
-    // console.log($('.form').val());
+$('#submit').on('click', () => {
+    const offset = $('#exampleFormControlSelect1').offset().top - 35;
+    console.log(offset);
+    window.scrollTo({ top: offset, behavior: 'smooth' });
 });
+
+
+let x = document.getElementById('collect-button');
